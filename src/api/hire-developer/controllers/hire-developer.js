ce7f0c8a8426df920d6de41c194cc16f1460d968
AjayShanmugasundram <ajay.shanmugasundram@agilisium.com>
2022-07-13 17:46:28 +0530

    Added my project

---
+++ src/api/hire-developer/controllers/hire-developer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  hire-developer controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::hire-developer.hire-developer');
---
+++ src/api/hire-developer/routes/hire-developer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hire-developer router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::hire-developer.hire-developer');
